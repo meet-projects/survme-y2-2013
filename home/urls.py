faaa99b3922eb1d43732566f1af9b259f4eb4503
Ron Lipel <ronl12@cs.huji.ac.il>
2013-07-01 16:52:05 +0300

    urls removed from home app

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-import views
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',)
